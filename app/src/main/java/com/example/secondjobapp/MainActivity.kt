4e8216d43f375b760b1ed84ea0f2b881ff5a32b1
thyspeter <52890767+thyspeter@users.noreply.github.com>
2022-07-10 22:38:39 +0200

    tijd toegevoegd

--- app/src/main/java/com/example/secondjobapp/MainActivity.kt
+++ app/src/main/java/com/example/secondjobapp/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.secondjobapp
 
+import android.os.Build
 import android.os.Bundle
 import android.os.Parcel
 import android.os.Parcelable
 import android.view.Menu
+import android.view.View
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.navigation.NavigationView
 import androidx.navigation.findNavController
@@ -16,22 +20,40 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 
 import com.example.secondjobapp.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.activity_main.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @AndroidEntryPoint
 
 class MainActivity() : AppCompatActivity() {
-
-
+    lateinit var bottomNav: BottomNavigationView
+    private lateinit var dateText: TextView
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        dateText = findViewById(R.id.text_date)
+
+        val formatter = DateTimeFormatter.ofPattern("dd-mm-YYYY HH:mm:ss")
+        val dateOfJob = formatter.format(LocalDateTime.now())
+
+
+        dateText.text = "Hallo"
         setSupportActionBar(toolbar)
 
         bottomNavigationView.setupWithNavController(navHostFragment.findNavController())
+        navHostFragment.findNavController()
+            .addOnDestinationChangedListener { _, destination, _ ->
+                navHostFragment.findNavController()}
+
 
     }
-
 }
+
+
+
+
--- app/src/main/java/com/example/secondjobapp/ui/fragments/JobFragment.kt
+++ app/src/main/java/com/example/secondjobapp/ui/fragments/JobFragment.kt
@@ -1,23 +1,51 @@
 package com.example.secondjobapp.ui.fragments
 
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.secondjobapp.R
 import com.example.secondjobapp.R.drawable
 import com.example.secondjobapp.ui.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.fragment_job.*
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 
 @AndroidEntryPoint
 class JobFragment : Fragment(R.layout.fragment_job) {
 
-    private val viewModel : MainViewModel by viewModels()
+    private lateinit var dateText: TextView
+
+    private val viewModel: MainViewModel by viewModels()
+
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        val formatterDate = DateTimeFormatter.ofPattern("dd-MM-YYYY")
+        val formatterTime = DateTimeFormatter.ofPattern("HH:mm:ss")
+        val start = LocalDateTime.now()
+        val dateOfJob = formatterDate.format(LocalDateTime.now())
+        val startTime = formatterTime.format(start)
 
+
+        text_date.text = "$dateOfJob"
+
+        btn_start.setOnClickListener() {
+            btn_start.text = "$startTime"
+        }
+        val end = LocalDateTime.now()
+        val endTime = formatterTime.format(end)
+        btn_end.setOnClickListener() {
+            btn_end.text = "$endTime"
+        }
 
     }
+
 }
