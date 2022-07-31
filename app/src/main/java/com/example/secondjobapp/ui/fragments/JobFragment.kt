673afcc6b9fbd2a508ffa727d6fae5ef85b0ef1f
Wouter <info@wouterthys.be>
2022-07-31 15:27:09 +0200

    Verschil in datums

--- app/src/main/java/com/example/secondjobapp/ui/fragments/JobFragment.kt
+++ app/src/main/java/com/example/secondjobapp/ui/fragments/JobFragment.kt
@@ -3,21 +3,19 @@ package com.example.secondjobapp.ui.fragments
 import android.os.Build
 import android.os.Bundle
 import android.view.View
-import android.widget.TextView
 import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.example.secondjobapp.R
-import com.example.secondjobapp.R.drawable
 import com.example.secondjobapp.ui.viewModels.MainViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.fragment_job.*
+import java.lang.String
 import java.lang.System.currentTimeMillis
 import java.text.SimpleDateFormat
+import java.time.Duration
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
-import java.util.logging.SimpleFormatter
 
 
 @AndroidEntryPoint
@@ -39,20 +37,25 @@ class JobFragment : Fragment(R.layout.fragment_job) {
 
         btn_start.setOnClickListener() {
 
-            val formatTime = SimpleDateFormat("hh:mm:ss")
+            val formatTime = SimpleDateFormat("HH:mm:ss")
             startTime = currentTimeMillis()
             val formattedStartTime = formatTime.format(startTime)
             btn_start.text = "start at $formattedStartTime"
         }
 
         btn_end.setOnClickListener() {
-            val formatTime = SimpleDateFormat("hh:mm:ss")
+            val formatTime = SimpleDateFormat("HH:mm:ss")
             endTime = currentTimeMillis()
             val formattedEndTime = formatTime.format(endTime)
             btn_end.text = "end at $formattedEndTime"
             val total = endTime - startTime
-            val formattedTotal = formatTime.format(total)
-            text_result.text = "total job is ${formattedTotal}"
+
+            val seconds = total / 1000
+            val minutes = seconds / 60
+            val hours = minutes / 60
+            val days = hours / 24
+
+            text_result.text = "total job is ${hours}:${minutes}:${seconds}"
 
 
         }
